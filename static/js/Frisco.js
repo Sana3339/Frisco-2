1d68fece69212191a5ec795aa3ac46859952e5ac
Sana <sanaahmad393@gmail.com>
2021-04-09 20:12:56 +0000

    Added event handlers to buttons on neighborhood.html

--- static/js/Frisco.js
+++ static/js/Frisco.js
@@ -7,3 +7,17 @@
 $('.enter-site').on('click', () => {
   window.location.href='/map';
 })
+
+$('#find-housing').on('click', () => {
+  alert('Take to find housing page!');
+})
+
+$('#post-housing').on('click', () => {
+  alert('You must create an account to post housing');
+  window.location.href='/login';
+})
+
+$('#back').on('click', () => {
+  window.location.href='/map';
+})
+
